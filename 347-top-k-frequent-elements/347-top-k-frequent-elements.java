e8b2a55581be220cd97aa5cbe4abeb0b4b7e2278
Anugrah Singhal <anugrahsinghal1@gmail.com>
2022-08-03 23:45:12 +0530

    Time: 38 ms (7.24%), Space: 50.3 MB (68.10%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,8 +1,7 @@
+/*Generics
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // use max-PQ
-        // pop K times
-        // nlogn
+        
         int n = nums.length;
         Map<Integer,Integer> map = new HashMap<>();
         
@@ -10,15 +9,21 @@ class Solution {
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
         
-        PriorityQueue<Pair<Integer,Integer>> pq = 
-            new PriorityQueue<>(Comparator.comparing(Pair<Integer,Integer>::value).reversed());
+        PriorityQueue<Pair<Integer,Integer>> MIN_HEAP = 
+            new PriorityQueue<>(Comparator.comparing(Pair<Integer,Integer>::value));
         
-        map.forEach((key,v) -> pq.offer(new Pair<>(key, v)));
+        map.forEach((key,v) -> {
+            MIN_HEAP.offer(new Pair<>(key, v));
+            if(MIN_HEAP.size() > k) {
+                MIN_HEAP.poll();
+            }
+        }
+        );
         
         
         int[] ans = new int[k];
         for(int i = 0; i < k; i++) {
-            ans[i] = pq.poll().key();
+            ans[i] = MIN_HEAP.poll().key();
         }
         
         return ans;
@@ -40,4 +45,51 @@ static class Pair<K,V> {
 }
 
 
+}
+*/
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        // number, freq
+        Map<Integer,Integer> freq = new HashMap<>();
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        PriorityQueue<Pair> minHeap = new PriorityQueue<>((p1,p2) -> p1.freq - p2.freq);
+        
+        for(Map.Entry<Integer,Integer> entry : freq.entrySet()) {
+            int number = entry.getKey();
+            int _freq = entry.getValue();
+            
+            minHeap.add(new Pair(number, _freq));
+            if(minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        
+        // System.out.println(minHeap);
+        
+        int[] res = new int[k];
+        for(int i = 0; i < k; i++) {
+            res[i] = minHeap.poll().elem;
+        }
+        
+        return res;
+    }
+    
+    static class Pair {
+         int elem;
+         int freq;
+         Pair(int e, int i) {
+               elem = e;
+               freq = i;
+               // System.out.printf("e %d - freq %d\n",elem,freq);
+         }
+        
+        public String toString() {
+            return String.format("e %d - freq %d\n",elem,freq);
+        }
+    }
 }
