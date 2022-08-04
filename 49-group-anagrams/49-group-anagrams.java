b98a29a5f3fc19dd30c68333d8393824975c41bc
Anugrah Singhal <anugrahsinghal1@gmail.com>
2022-08-04 19:34:08 +0530

    Time: 18 ms (34.39%), Space: 55.3 MB (79.11%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -4,13 +4,16 @@ class Solution {
         for(var origStr : strs) {
             char[] arr = origStr.toCharArray();
             Arrays.sort(arr);
+            
             var str = new String(arr);
-            if(map.get(str) == null) {
-                map.put(str, new ArrayList<>());
-            }
+            
+            map.putIfAbsent(str, new ArrayList<>());
+
             map.get(str).add(origStr);
         }
         
         return new ArrayList<>(map.values());
     }
 }
+
+
