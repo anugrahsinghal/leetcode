f2c7053f425d1ae94ade47db4c274de2ef2e3061
Anugrah Singhal <anugrahsinghal1@gmail.com>
2022-08-19 15:16:21 +0530

    Time: 1 ms (99.61%), Space: 40.5 MB (90.17%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -27,8 +27,8 @@ class Solution {
             }
         }
         
-        if(!stack.isEmpty()) return false;
+        // if(!stack.isEmpty()) return false;
         
-        return true;
+        return stack.isEmpty();
     }
 }
