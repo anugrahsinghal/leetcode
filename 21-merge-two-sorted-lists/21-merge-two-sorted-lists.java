8430c00a9f061d8da5a427c632a49b4eb9ac1b3c
Anugrah Singhal <anugrahsinghal1@gmail.com>
2022-08-19 16:12:20 +0530

    Time: 1 ms (80.10%), Space: 41.9 MB (92.04%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -10,54 +10,13 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null && l2 == null) {
-            return l1;
-        } 
-        if(l1 == null) {
-            return l2;
-        }
-        if(l2 == null) {
-            return l1;
-        }
-        
         ListNode dummy = new ListNode(0);
         var dummyHead = dummy;
         
-        /*
-        a
-        1->2->4
-        b
-        1->3->4
-        
-        
-        dummy = 0
-        
-        if(a <= b) {1<=1
-            dummy.next = l1;0-1
-            dummy = dummy.next;
-            
-            l1 = l1.next;
-            dummy.next = null;
-        }
-        
-        */
-        
-        while(l1 != null || l2 != null) {
-            int a = l1 == null ? -101 : l1.val;//2
-            int b = l2 == null ? -101 : l2.val;//1
-            // a = null, b != null
-            // a != null, b = null
-            if(b == -101) {
-                dummy.next = l1;
-                dummy = dummy.next;    
-                l1 = l1.next;
-                dummy.next = null;
-            } else if(a == -101) {
-                dummy.next = l2;
-                dummy = dummy.next;    
-                l2 = l2.next;
-                dummy.next = null;
-            } else if(a <= b) {
+        while(l1 != null && l2 != null) {
+            int a = l1.val;
+            int b = l2.val;
+            if(a <= b) {
                 dummy.next = l1;
                 dummy = dummy.next;    
                 l1 = l1.next;
@@ -69,18 +28,13 @@ class Solution {
                 dummy.next = null;
             }
         }
+        if(l1 != null) {
+            dummy.next = l1;
+        } else if(l2 != null) {
+            dummy.next = l2;
+        }
         
         return dummyHead.next;
     }
     
-    
-    // void print(ListNode l) {
-    //     var temp = l;
-    //     while(temp != null) {
-    //         System.out.print(temp.val);
-    //         System.out.print("->");
-    //         temp = temp.next;
-    //     }
-    //     System.out.println("null");
-    // }
 }
