1d83c2123b98c516269ac6d15c9cf03d8f3ba80d
Anugrah Singhal <anugrahsinghal1@gmail.com>
2022-08-19 19:09:05 +0530

    Time: 3 ms (94.90%), Space: 42 MB (99.71%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -9,11 +9,11 @@ class Solution {
         var s2 = t.toCharArray();
         Arrays.sort(s2);
         
-        for(int i = 0; i < s.length(); i++) {
-            if(s1[i] != s2[i])
-                return false;
-        }
+        // for(int i = 0; i < s.length(); i++) {
+        //     if(s1[i] != s2[i])
+        //         return false;
+        // }
         
-        return true;
+        return Arrays.equals(s1, s2);
     }
 }
