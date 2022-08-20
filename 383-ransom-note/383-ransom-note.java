07bb65e4893b11963a22d2ab426559a4d29ad1dd
Anugrah Singhal <anugrahsinghal1@gmail.com>
2022-08-21 00:11:02 +0530

    Time: 30 ms (30.56%), Space: 51.6 MB (37.52%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -2,20 +2,33 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         if(ransomNote.length() > magazine.length()) return false;
         // does magazine have equal letters as ransom-note
-        var mag = freqMap(magazine);
-        var note = freqMap(ransomNote);
-        if(note.size() > mag.size()) return false;
-        
-        for(Character letter : note.keySet()) {
-            int freq = note.get(letter);
-            
-            Integer magFreq = mag.get(letter);
-            if(magFreq == null || magFreq < freq) {
+        var map = freqMap(magazine);
+        var n = ransomNote.length();
+        for(int i = 0; i < n; i++) {
+            char c = ransomNote.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) - 1);
+            if(map.get(c) < 0) {
                 return false;
             }
         }
         
         return true;
+        
+        
+        
+//         var note = freqMap(ransomNote);
+//         if(note.size() > mag.size()) return false;
+        
+//         for(Character letter : note.keySet()) {
+//             int freq = note.get(letter);
+            
+//             Integer magFreq = mag.get(letter);
+//             if(magFreq == null || magFreq < freq) {
+//                 return false;
+//             }
+//         }
+        
+//         return true;
     }
     
     Map<Character, Integer> freqMap(String s) {
