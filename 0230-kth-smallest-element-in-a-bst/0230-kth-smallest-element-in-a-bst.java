57d09bb2857ec963913f3fdaae8fefdb312a9c9b
Anugrah Singhal <anugrahsinghal1@gmail.com>
2023-08-23 07:55:59 +0530

    Time: 0 ms (100%), Space: 44.5 MB (22.1%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,18 +14,31 @@
  * }
  */
 class Solution {
+    // normally works
+    // But Follow up: ??
+    //   If the BST is modified often (i.e., we can do insert and delete operations) 
+    //   and you need to find the kth smallest frequently, how would you optimize?
+
+
     public int kthSmallest(TreeNode root, int k) {
-        List<Integer> arr = new ArrayList<>();
-        inOrder(root, arr);
+        int[] count = new int[]{0};
+        int[] ans = new int[1];
+        inOrder(root, count, ans, k);
         
-        return arr.get(k-1);
+        return ans[0];
     }
 
-    public void inOrder(TreeNode node, List<Integer> arr) {
+    public void inOrder(TreeNode node, int[] count, int[] ans, int k) {
         if(node == null) return;
 
-        inOrder(node.left, arr);
-        arr.add(node.val);
-        inOrder(node.right, arr);
+        inOrder(node.left, count, ans, k);
+        
+        count[0]++;
+        if(count[0] == k) {
+            ans[0] = node.val;
+            return;// no need to do more recursion
+        }
+
+        inOrder(node.right, count, ans, k);
     }
 }
