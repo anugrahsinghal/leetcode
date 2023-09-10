ddca10890501d234d99f3c72ee16c50e9cd84036
Anugrah Singhal <anugrahsinghal1@gmail.com>
2023-09-10 16:16:06 +0530

    Time: 120 ms (30.06%), Space: 58.7 MB (25.98%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,10 +1,5 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        // for each index try the next greater to right and calc area
-        // for each index try the next greater to left and calc area
-        // and also area of each node or for where next is -1
-        // System.out.println(Arrays.toString(nextSmallestToRight(heights)));
-        // System.out.println(Arrays.toString(nextSmallestToLeft(heights)));
         var h = heights;
         var maxH = 0;
         var n = h.length;
@@ -12,33 +7,88 @@ class Solution {
         var nsl = nextSmallestToLeft(heights);
         for(int i = 0; i < n; i++) { 
             maxH = Math.max(maxH, (nsr[i] - nsl[i] - 1) * heights[i]);
-            // var currH = h[i];
-            // maxH = Math.max(maxH, currH);
+        }
+        return maxH;
+    }
 
-            // if(ngl[i] != -1) {
-            //     var leftHighestIdx = ngl[i];
-            //     var leftDist = i - leftHighestIdx + 1;
-            //     var leftH = leftDist * Math.min(currH, h[leftHighestIdx]);
-            //     System.out.printf("L-Dist %d - %d = %d || H = %d \n", i, leftHighestIdx, leftDist, leftH);
-            //     maxH = Math.max(maxH, leftH);
-            // }
-            // if(ngr[i] != -1) {
-            //     var rightHighestIdx = ngr[i];
-            //     var rightDist = rightHighestIdx - i + 1;
-            //     var rightH = rightDist * Math.min(currH, h[rightHighestIdx]);
-            //     System.out.printf("R-Dist %d - %d = %d || H = %d \n", rightHighestIdx, i,rightDist, rightH);
-            //     maxH = Math.max(maxH, rightH);
-            // }
+    int[] nextSmallestToLeft(int[] arr) {
+        int n = arr.length;
+        int[] res = new int[n];
+        Stack<Pair<Integer,Integer>> st = new Stack<>();
+        // key = elem
+        // value = index
+        for(int i = 0; i < n; i++) { 
+            while(!st.isEmpty() && st.peek().key >= arr[i]) {
+                st.pop();
+            }
+            if(st.isEmpty()) {
+                res[i] = -1;
+            } else {
+                res[i] = st.peek().value;
+            }
+            st.push(new Pair<>(arr[i], i));
+        }
+        return res;
+    }
+    int[] nextSmallestToRight(int[] arr) {
+        int n = arr.length;
+        int[] res = new int[n];
+        Stack<Pair<Integer,Integer>> st = new Stack<>();
+        // key = elem
+        // value = index
+        for(int i = n-1; i >= 0; i--) { 
+            while(!st.isEmpty() && st.peek().key >= arr[i]) {
+                st.pop();
+            }
+            if(st.isEmpty()) {
+                res[i] = n;
+            } else {
+                res[i] = st.peek().value;
+            }
+            st.push(new Pair<>(arr[i], i));
+        }
+        return res;
+    }
 
-            // int width = 0;
-            // if(ngr[i] != -1) {
-            //     width = ngr[i] - i;
-            // }
-            // if(ngl[i] != -1) {
-            //     width = Math.max(width, i - ngl[i]);
-            // }
+    public int largestRectangleArea_OLD(int[] heights) {
+        // for each index try the next greater to right and calc area
+        // for each index try the next greater to left and calc area
+        // and also area of each node or for where next is -1
+        System.out.println(Arrays.toString(nextSmallestToRight(heights)));
+        System.out.println(Arrays.toString(nextSmallestToLeft(heights)));
+        var h = heights;
+        var maxH = 0;
+        var n = h.length;
+        var ngr = nextSmallestToRight(heights);
+        var ngl = nextSmallestToLeft(heights);
+        for(int i = 0; i < n; i++) { 
+            var currH = h[i];
+            maxH = Math.max(maxH, currH);
 
-            // maxH = Math.max(maxH, (width + 1) * currH);
+            if(ngl[i] != -1) {
+                var leftHighestIdx = ngl[i];
+                var leftDist = i - leftHighestIdx + 1;
+                var leftH = leftDist * Math.min(currH, h[leftHighestIdx]);
+                System.out.printf("L-Dist %d - %d = %d || H = %d \n", i, leftHighestIdx, leftDist, leftH);
+                maxH = Math.max(maxH, leftH);
+            }
+            if(ngr[i] != -1) {
+                var rightHighestIdx = ngr[i];
+                var rightDist = rightHighestIdx - i + 1;
+                var rightH = rightDist * Math.min(currH, h[rightHighestIdx]);
+                System.out.printf("R-Dist %d - %d = %d || H = %d \n", rightHighestIdx, i,rightDist, rightH);
+                maxH = Math.max(maxH, rightH);
+            }
+
+            int width = 0;
+            if(ngr[i] != -1) {
+                width = ngr[i] - i;
+            }
+            if(ngl[i] != -1) {
+                width = Math.max(width, i - ngl[i]);
+            }
+
+            maxH = Math.max(maxH, (width + 1) * currH);
         }
 
         return maxH;
@@ -89,45 +139,6 @@ ngl=[-1,0,-1,-1,3,3]
             st.push(new Pair(arr[i], i));
         }
         return ngl;
-    }
-
-    int[] nextSmallestToLeft(int[] arr) {
-        int n = arr.length;
-        int[] res = new int[n];
-        Stack<Pair<Integer,Integer>> st = new Stack<>();
-        // key = elem
-        // value = index
-        for(int i = 0; i < n; i++) { 
-            while(!st.isEmpty() && st.peek().key >= arr[i]) {
-                st.pop();
-            }
-            if(st.isEmpty()) {
-                res[i] = -1;
-            } else {
-                res[i] = st.peek().value;
-            }
-            st.push(new Pair<>(arr[i], i));
-        }
-        return res;
-    }
-    int[] nextSmallestToRight(int[] arr) {
-        int n = arr.length;
-        int[] res = new int[n];
-        Stack<Pair<Integer,Integer>> st = new Stack<>();
-        // key = elem
-        // value = index
-        for(int i = n-1; i >= 0; i--) { 
-            while(!st.isEmpty() && st.peek().key >= arr[i]) {
-                st.pop();
-            }
-            if(st.isEmpty()) {
-                res[i] = n;
-            } else {
-                res[i] = st.peek().value;
-            }
-            st.push(new Pair<>(arr[i], i));
-        }
-        return res;
     }
 
 static class Pair<K,V> {
