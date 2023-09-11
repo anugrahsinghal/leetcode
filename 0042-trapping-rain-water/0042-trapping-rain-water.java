a8d5ac4c0524c6b88a86e31ecb97f40e698fed51
Anugrah Singhal <anugrahsinghal1@gmail.com>
2023-09-11 13:08:48 +0530

    Time: 1 ms (81.14%), Space: 44.3 MB (60.77%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -3,17 +3,36 @@ class Solution {
         // 2 techniques
         // technique 1
         // 2pointer(harder intuition)
+        var arr = height;
+        int n = arr.length;
+        int l = 0;
+        int r = n-1;
+        int lmax = arr[l];
+        int rmax = arr[r];
+        int water = 0;
+        while(l < r) {
+            if (lmax <= rmax) {
+                l++;
+                lmax = Math.max(lmax, arr[l]);
+                water += Math.max(0, lmax - height[l]);//avoid negatives
+            } else {
+                r--;
+                rmax = Math.max(rmax, arr[r]);
+                water += Math.max(0, rmax - height[r]);//avoid negatives
+            }
+        }
+        return water;
         // technique 2
         // min for each point and water stored
         // no need to calculate max water we only need max water at current point
-        var maxToLeft = maxToLeft(height);
-        var maxToRight = maxToRight(height);
-        var minLR = minLR(maxToLeft, maxToRight, height);
-        int water = 0;
-        for(int i = 0; i < height.length; i++) { 
-            water += Math.max(0, minLR[i] - height[i]);
-        }
-        return water;
+        // var maxToLeft = maxToLeft(height);
+        // var maxToRight = maxToRight(height);
+        // var minLR = minLR(maxToLeft, maxToRight, height);
+        // int water = 0;
+        // for(int i = 0; i < height.length; i++) { 
+        //     water += Math.max(0, minLR[i] - height[i]);
+        // }
+        // return water;
     }
     int[] maxToLeft(int[] arr) {
         int n = arr.length;
@@ -22,7 +41,7 @@ class Solution {
         for(int i = 1; i < n; i++) { 
             res[i] = Math.max(arr[i-1], res[i-1]);
         }
-        System.out.println(Arrays.toString(res));
+        // System.out.println(Arrays.toString(res));
         return res;
     }
     int[] maxToRight(int[] arr) {
@@ -32,7 +51,7 @@ class Solution {
         for(int i = n-2; i >= 0; i--) { 
             res[i] = Math.max(arr[i+1], res[i+1]);
         }
-        System.out.println(Arrays.toString(res));
+        // System.out.println(Arrays.toString(res));
         return res;
     }
     int[] minLR(int[] left,int[]  right,int[]  arr) {
@@ -41,7 +60,7 @@ class Solution {
         for(int i = 0; i < n; i++) { 
             res[i] = Math.min(left[i], right[i]);
         }
-        System.out.println(Arrays.toString(res));
+        // System.out.println(Arrays.toString(res));
         return res;
     }
 }
