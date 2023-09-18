72189fdfa97fc099da040e17c0c15100ff00b395
Anugrah Singhal <anugrahsinghal1@gmail.com>
2023-09-19 03:11:48 +0530

    Time: 63 ms (27.33%), Space: 44.6 MB (23.92%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String minWindow(String s, String t) {
+      Map<Character, Integer> map = new HashMap<>();
+      for(int i = 0; i < t.length(); i++) { 
+        char c = t.charAt(i);
+        map.put(c, map.getOrDefault(c, 0) + 1);
+      }
+      
+      int i = 0, j = 0, min = Integer.MAX_VALUE;
+      int uniqCount = map.size();
+      String ans = "";
+      while(j < s.length()) {
+        char c = s.charAt(j);
+        if(map.containsKey(c)) {
+          map.put(c, map.get(c) - 1);
+          if(map.get(c) == 0) {
+            uniqCount--;
+          }
+        }
+        // if valid string found
+        if(uniqCount == 0) {// increment I till we make smallest substring
+          while(uniqCount == 0 && i <= j) {
+            // System.out.println(map);
+            // System.out.println(s.substring(i, j+1));
+            if(j-i+1 < min) {
+              min = j - i + 1;
+              ans = s.substring(i, j+1);
+            }
+            char start = s.charAt(i);
+            if(map.containsKey(start)) {
+              map.put(start, map.get(start) + 1);
+              if(map.get(start) > 0) {
+                uniqCount++;
+              }
+            }
+            i++;
+          }
+        }
+
+        j++;
+      }
+
+      return ans;
+    }
+}
