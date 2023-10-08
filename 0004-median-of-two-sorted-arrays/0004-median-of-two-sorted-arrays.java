2733816de61a32a60f051770fc3dacdeffc26bca
Anugrah Singhal <anugrahsinghal1@gmail.com>
2023-10-08 23:24:03 +0530

    Time: 2 ms (65.93%), Space: 44.3 MB (83.89%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,68 +1,59 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        var total = nums1.length + nums2.length;
+
+    /*
+        // [1,3] [2]
+        // half = 2
+        // numB = 0+1/2=0, numA = 2-0=2
+        //   1,3|+INF
+        //  -INF|2
+        // --incrase numElementsB
+        // numB = 1+1/2=1, numA = 2-1=1
+        //    1 la | ra 3
+        //    2 lb | rb +INF
+    */
+    public double findMedianSortedArrays(int[] a, int[] b) {
+        var total = a.length + b.length;
         var half = (total + 1)/2;
 
-        int[] a;
-        int[] b;
-        if(nums1.length > nums2.length) {
-            a = nums1;
-            b = nums2;
-        } else {
-            b = nums1;
-            a = nums2;
+        if(a.length < b.length) {
+            return findMedianSortedArrays(b, a);
         }
 
-        var bleft  = 0;
-        var bright = b.length;
+        var bMinElements  = 0;
+        var bMaxElements = b.length;
+        // BINARY SEARCH OVER HOW MANY ELEMENTS OF B[] we will choose
 
-        // var aleft  = 0;
-        // var aright = a.length - 1;
-        // System.out.println((int)Math.ceil(-1/(double)2));
-        
         while(true) {
-            int numElementsB = (bleft + bright) / 2;// elements b4 mid are chosen
-            // System.out.printf("bl = %d, br = %d, numEle = %d\n", bleft, bright, numElementsB);
-
+            int numElementsB = (bMinElements + bMaxElements) / 2;
             int numElementsA = half - numElementsB;
 
             // System.out.printf("bl = %d, br = %d, numB = %d, numA = %d\n", bleft, bright, numElementsB, numElementsA);
-            // [1,3] [2]
-            // half = 2
-            // numB = 0+1/2=0, numA = 2-0=2
-            //   1,3|+INF
-            //  -INF|2
-            // --incrase numElementsB
-            // numB = 1+1/2=1, numA = 2-1=1
-            //    1 |3
-            //    2 |+INF
 
-            // 1,2 la | ra 3,4 
-            //   5 lb | rb 6
-            var lb = numElementsB - 1;
-            var la = numElementsA - 1;
+            var leftBIdx = numElementsB - 1;
+            var leftAIdx = numElementsA - 1;
 
-            var rb = numElementsB;
-            var ra = numElementsA;
+            var rightBIdx = numElementsB;
+            var rightAIdx = numElementsA;
 
-            var lbVal = numElementsB - 1 >= 0 ? b[numElementsB - 1] : Integer.MIN_VALUE;
-            var laVal = numElementsA - 1 >= 0 ? a[numElementsA - 1] : Integer.MIN_VALUE;
+            var lbVal = leftBIdx >= 0 ? b[numElementsB - 1] : Integer.MIN_VALUE;
+            var laVal = leftAIdx >= 0 ? a[numElementsA - 1] : Integer.MIN_VALUE;
 
-            var rbVal = numElementsB < b.length ? b[numElementsB] : Integer.MAX_VALUE;
-            var raVal = numElementsA < a.length ? a[numElementsA] : Integer.MAX_VALUE;
+            var rbVal = rightBIdx < b.length ? b[numElementsB] : Integer.MAX_VALUE;
+            var raVal = rightAIdx < a.length ? a[numElementsA] : Integer.MAX_VALUE;
 
             // System.out.printf("LA = %d, LB = %d, RA = %d, RB = %d\n", laVal, lbVal, raVal, rbVal);
 
+            // check in cross that left side both element are smaller
             if(lbVal <= raVal && laVal <= rbVal) {
-                if(total % 2 == 1) {//odd
+                if(total % 2 == 1) {
                     return Math.max(lbVal, laVal);
                 } else {
                     return (Math.max(laVal, lbVal) + Math.min(raVal, rbVal)) / 2.0;
                 }
             } else if (lbVal > raVal) {
-                bright = numElementsB-1;
+                bMaxElements = numElementsB - 1;
             } else if (laVal > rbVal) {
-                bleft = numElementsB+1;
+                bMinElements = numElementsB + 1;
             }
 
         }
