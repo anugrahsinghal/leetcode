0eae0a38d6986758fc03df8ac5c2248b4f7d604e
Anugrah Singhal <anugrahsinghal1@gmail.com>
2023-10-10 17:10:46 +0530

    Time: 0 ms (100%), Space: 40.3 MB (13.9%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -7,16 +7,16 @@ class Solution {
     }
 
     static int helper(int[] arr, int idx, int[] dp) {
-		if(idx < 0) return 0;
-		if(idx == 0) return arr[0];	
-		if(dp[idx] != -1) return dp[idx];
+			if(idx < 0) return 0;
+			if(idx == 0) return arr[0];	
+			if(dp[idx] != -1) return dp[idx];
 
-		// cant choose adjacent so idx-2
-		int pick = arr[idx] + helper(arr, idx-2, dp);
-		int notPick = 0 + helper(arr, idx-1, dp);
+			// cant choose adjacent so idx-2
+			int pick = arr[idx] + helper(arr, idx-2, dp);
+			int notPick = 0 + helper(arr, idx-1, dp);
 
-		dp[idx] = Math.max(pick, notPick);
+			dp[idx] = Math.max(pick, notPick);
 
-		return dp[idx];
-	}
+			return dp[idx];
+		}
 }
